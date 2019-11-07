14ff3fa20d732d6038f0cc457c1286a343f88336
Lucas <lucas.t.micheli@gmail.com>
2019-11-07 00:25:44 -0300

    terminando de documentar el codigo

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from threading import Timer
 
 connected_list = []
 
-#Funcion principal para encontrar funciones
+#Funcion principal para encontrar funciones. Nuestro controlador
 def atender(sock, msg, data):
 	lista = []
 	lista = data.split(" ")
@@ -13,26 +13,26 @@ def atender(sock, msg, data):
 		if opcion not in opciones:
 			sockfd.send("\n El comando no existe o es incorrecto. Use /help para ver los comandos disponibles")
 		else:
-			if opcion == opciones[1]: ###### LOGOUT ######
+			if opcion == opciones[0]: ###### LOGOUT ######
 				logout(sock)
-			elif opcion == opciones[2]: ###### MSG ######
+			elif opcion == opciones[1]: ###### MSG ######
 				if(len(lista) > 2):
 					user = lista [1]
 					param_dos = ' '.join(lista[2:])
 					privado(sock,param_dos,user)
 				else:
 					todos(sock, msg)
-			elif opcion == opciones[3]: ###### HELP ######
+			elif opcion == opciones[2]: ###### HELP ######
 				helps(sock)
-			elif opcion == opciones[4]: ###### NICK ######
+			elif opcion == opciones[3]: ###### NICK ######
 				if(len(lista) > 1):
 					newNick = lista [1]
 					nick(sock,newNick)
 
-			elif opcion == opciones[5]: ###### USUARIOS ######
+			elif opcion == opciones[4]: ###### USUARIOS ######
 				usuarios(sock)
 
-			elif opcion == opciones[6]: ###### KICK ######
+			elif opcion == opciones[5]: ###### KICK ######
 				if(len(lista) > 1):
 					user = lista [1]
 					param_dos = ' '.join(lista[2:])
@@ -44,7 +44,8 @@ def atender(sock, msg, data):
 	else:
 		todos(sock, msg)
 
-
+# Funcion para el logout recorrmos todos los usuarios y mandamos un mensaje a ellos
+# y luego ejecutamos una funcion para que se ejecute dentro de 10 seg
 def logout (sock):
 	for socket in connected_list:
 		if socket != server_socket:
@@ -58,13 +59,14 @@ def logout (sock):
 	t = Timer(10.0, timerLogout)
 	t.start()
 	
-
+# Funcion para el timeout del logout del servidor
 def timerLogout():
 	global flag
 	flag = 0
 
 
-#Funcion para enviar mensajes a todos
+#Funcion para enviar mensajes a todos. Recorremos la lista de conectados
+# y le enviamos un mensaje a todos los usuarios
 def todos (sock, message):
 	#El mensaje no se envia al servidor ni al 
 	for socket in connected_list:
@@ -76,15 +78,13 @@ def todos (sock, message):
 				socket.close()
 				connected_list.remove(socket)
 
+# se envia un mensaje privado a un usuario seleccionado. Recorremos la lista de conectados
+# y buscamos el usuario y le mandamos el mensaje solo a el
 def privado(sock,message,user):
 	llave = [key for key, nombre in record.items() if nombre == user]
-	#print llave
 	lla = llave[0]
 	for socket in connected_list:
 		if socket != server_socket:
-			#print "esto es socket de lista ", socket.getpeername()
-			#print "esta es la llave de ram ", lla
-			#print "la llave es igual alsocket? ", lla == socket.getpeername()
 			if socket.getpeername() == lla:
 				try :
 					if (sock.getpeername() == lla):
@@ -96,15 +96,13 @@ def privado(sock,message,user):
 					socket.close()
 					connected_list.remove(socket)
 
+# se elimina un usuario seleccionado. Recorremos la lista de conectados
+# y buscamos el que se quiere eliminar
 def kick(sock,message,user):
 	llave = [key for key, nombre in record.items() if nombre == user]
-	#print llave
 	lla = llave[0]
 	for socket in connected_list:
 		if socket != server_socket and socket != sock:
-			#print "esto es socket de lista ", socket.getpeername()
-			#print "esta es la llave de ram ", lla
-			#print "la llave es igual alsocket? ", lla == socket.getpeername()
 			if socket.getpeername() == lla:
 				try :
 					msg="\r\33[1m"+"\33[31m "+record[lla]+" ha sido eliminado por "+record[sock.getpeername()]+" \33[0m\n"
@@ -119,6 +117,7 @@ def kick(sock,message,user):
 					socket.close()
 					connected_list.remove(socket)
 
+# el usuario obtiene una lista de todos los usuarios conectados
 def usuarios(sock):
 	try :
 		lista_usuarios = ""
@@ -139,6 +138,9 @@ def usuarios(sock):
 		socket.close()
 		connected_list.remove(socket)
 
+# function: le pasamos el socket y el nuevo nick y recorremos el array de usuarios
+# preguntando si el nuevo nick es diferente a cualquier otro usuario
+# enviamos el mensaje a todos y al mismo usuario
 def nick(sock, newNick):
 	try :
 		oldNick = ""
@@ -147,14 +149,16 @@ def nick(sock, newNick):
 				oldNick = nombre
 				record[sock.getpeername()] = newNick
 		if(oldNick != ""):
-			msg = "\n El usuario: " + oldNick + " cambio su nick a " + newNick + "\n"
+			msg = "\n El usuario " + oldNick + " cambio su nick a " + newNick + "\n"
 			todos(sock, msg)
+			privado(sock, "Cambiaste tu nick a " + newNick, newNick)
 		
 	except :
 		# Si la conexion no esta disponible
 		socket.close()
 		connected_list.remove(socket)
-
+		
+# Funcion de help: se le envia la ayuda al usuario seleccionado
 def helps(sock):
 	try :
 		sock.send(ayuda)
@@ -171,17 +175,19 @@ if __name__ == "__main__":
 	record={}
 	# lista para mantener descriptores de sockets
 	connected_user = []
-
+	#Se usa para el logout, si es 0 apagamos el servidor 
 	flag = 1
-
-	opciones = ["login","logout","msg","help","nick","usuarios","kick"]
+	#lista de opciones para los codigos
+	opciones = ["logout","msg","help","nick","usuarios","kick"]
 
 	ayuda = "\33[32m\t\t **************** AYUDA **************** \n"
 	ayuda += "\t/msg 'usuario' 'mensaje': Mensaje privado a usuario\n"
 	ayuda += "\t/nick 'nuevo nick': Cambiar nick\n"
 	ayuda += "\t/usuarios : Lista de usuarios conectados\n"
 	ayuda += "\t/kick 'usuario' : Elimina un usuario\n"
-	ayuda += "\t/help : Ayuda\33[0m\n"
+	ayuda += "\t/help : Ayuda\n"
+	ayuda += "\texit : Salir\n"
+	ayuda += "\t/logout : Apagar servidor\n"
 	ayuda += "\33[32m\t\t *************************************** \n"
 
 	buffer = 4096
@@ -222,11 +228,9 @@ if __name__ == "__main__":
 					record[addr]=name
 					print "Cliente (%s, %s) conectado" % addr," [",record[addr],"]"
 					
-					sockfd.send("\33[32m\r\33[1m Bienvenido al Chat. Ingrese 'tata' en cualquier momento para salir \n\33[0m")
+					sockfd.send("\33[32m\r\33[1m Bienvenido al Chat. Ingrese 'exit' en cualquier momento para salir \n\33[0m")
 					todos(sockfd, "\33[32m\33[1m\r "+name+" se unio a la conversacion \n\33[0m")
-					#send_to_user(sockfd, "esto es una prueba si sos ram")
-
-			#Mensajes entrantes de algun cliente
+			#Mensajes entrantes de cualquier usuario que no sea la primera conexion
 			else:
 				# Datos del cliente
 				try:
@@ -236,10 +240,13 @@ if __name__ == "__main__":
                     
                     #obtener addr del cliente que envia el mensaje
 					i,p=sock.getpeername()
-					if data == "tata":
+					if data == "exit":
 						msg="\r\33[1m"+"\33[31m "+record[(i,p)]+" ha dejado la conversacion \33[0m\n"
+						#enviamos la desconexion del usuario a los demas usuarios
 						todos(sock,msg)
+						#logueamos la desconexion del usuario en el servidor
 						print "Cliente (%s, %s) esta offline" % (i,p)," [",record[(i,p)],"]"
+						#eliminamos el usuario de la lista y eliminamos el socket de la lista de socket
 						del record[(i,p)]
 						connected_list.remove(sock)
 						sock.close()
@@ -247,6 +254,10 @@ if __name__ == "__main__":
 
 					else:
 						msg="\r\33[1m"+"\33[35m "+record[(i,p)]+": "+"\33[0m"+data+"\n"
+						#es nuestro controlador, donde manejamos los codigos
+						#sock: es el socket abierto entre el servidor y el usuario
+						#msg: es el mensaje armado para que salga lindo en la pantalla
+						#data: es el mensaje crudo que envio el cliente
 						atender(sock,msg,data)
             
                 #Si hay algun problema se deconecta el usuario
